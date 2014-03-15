6eb115519ddc76a510837e6f35e6e59e38640561
Ben Meier <benmeier42@gmail.com>
2014-03-15 06:10:33 +0200

    added console logs, added sample image

--- capture.rb
+++ capture.rb
@@ -24,35 +24,51 @@ img = "img#{t}.jpg"
 thumb = "thumb#{t}.jpg"
 img_path = File.join(temp_dir, img)
 thumb_path = File.join(temp_dir, thumb)
+sample_path = File.join(current_dir, 'noimage.jpg')
 remote_img_path = File.join(cnf['remoteimgfolder'], '')
 remote_script_path = File.join(cnf['remotescriptfolder'], '')
 
 # remove any existing images
+puts "Deleting existing images in #{temp_dir}"
 Dir.glob(File.join(temp_dir, '*.jpg')) do |f|
   File.delete(f)
 end
 
 # capture image
-stdout,stderr,status = Open3.capture3("raspistill -o #{img_path} -w #{cnf['imagesize']['width']} -h #{cnf['imagesize']['height']} -q 75 -n")
+puts "Capturing #{cnf['imagesize']['width']}x#{cnf['imagesize']['height']} image to #{img_path}"
+begin
+  stdout,stderr,status = Open3.capture3("raspistill -o #{img_path} -w #{cnf['imagesize']['width']} -h #{cnf['imagesize']['height']} -q 75 -n")
+rescue
+  puts 'Camera not available. Copying non existant image'
+  Magick::Image.read(sample_path).first.write(img_path)
+end
 
 # create thumbnail
+puts "Generating thumbnail #{img_path} -> #{thumb_path}"
 image = Magick::Image.read(img_path).first
 resized = image.resize_to_fit(cnf['thumbsize']['width'], cnf['thumbsize']['height'])
 resized.write(thumb_path)
 
 # ensure folder exists
+puts "Creating ssh connection to #{cnf['username']}@#{cnf['remote']}"
 Net::SSH.start(cnf['remote'], cnf['username'], :password => cnf['password'] ) do |ssh|
+  puts "Creating remote folder #{remote_img_path}"
   ses = ssh.exec!("mkdir #{remote_img_path}")
 end
 
 # scp onto server
+puts "Creating scp connection to #{cnf['username']}@#{cnf['remote']}"
 Net::SCP.start(cnf['remote'], cnf['username'], :password => cnf['password'] ) do |scp|
+  puts 'Uploading img'
   scp.upload!(img_path, remote_img_path)
+  puts 'Uploading thumb'
   scp.upload!(thumb_path, remote_img_path)
 end
 
 # start script
 script = File.join(remote_script_path, 'rebuild_img_listing.rb')
+puts "Creating ssh connection to #{cnf['username']}@#{cnf['remote']}"
 Net::SSH.start(cnf['remote'], cnf['username'], :password => cnf['password'] ) do |ssh|
+  puts "Executing script #{script}"
   ses = ssh.exec!("ruby #{script}")
 end
